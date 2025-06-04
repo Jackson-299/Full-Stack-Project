60f2007c4e76bed4b8e4d3dd5e39fde930ba479f
Jackson-299 <havocjack2942002@gmail.com>
2025-06-04 12:11:07 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line to match your project's structure
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line to match your project's structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
